42feb0936e2a0a5fe7be08e25140c660b9ed774c
AjaxGb <ajaxgb@gmail.com>
2018-12-14 22:15:29 -0500

    Fix default log file not working
    If you put `-l` by itself, it used to be ignored. Now it uses `log.txt`
    as the default log file, as intended.

--- main.py
+++ main.py
@@ -645,7 +645,7 @@ class BlackjackSimulator:
                 self.player_ai.end_round(0)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     import argparse
     
     parser = argparse.ArgumentParser()
@@ -678,14 +678,14 @@ if __name__ == '__main__':
         action="store_true")
     parser.add_argument("-l", "--log",
         help="save logs to a file",
-        nargs="?", default=None)
+        nargs="?", const="log.txt")
     args = parser.parse_args()
     
     
-    if args.log == None:
+    if args.log is None:
         log_file = None
     else:
-        log_file = open(args.log or "log.txt", "w", encoding='utf-8')
+        log_file = open(args.log, "w", encoding="utf-8")
     
     player_ai = args.ai_type()
     sim = BlackjackSimulator(player_ai,
